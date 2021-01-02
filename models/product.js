36d7a5063390b2c5c29e8253c58a00bcd50f6810
KanzaBasharat <Kanzabasharat5@gmail.com>
2021-01-02 15:57:03 +0500

    commit

--- models/product.js
+++ models/product.js
@@ -1,22 +1,11 @@
 var mongoose = require("mongoose");
-const Joi = require("@hapi/joi");
 
 var productSchema = mongoose.Schema({
+  id: String,
   name: String,
   price: String,
-  size: String,
   category: String,
+  details: String,
 });
 const Product = mongoose.model("Product", productSchema);
-
-function validateProduct(data) {
-	const schema = Joi.object({
-	  name: Joi.string().min(3).max(20).required(),
-	  price: Joi.number().min(0).required(),
-    size: Joi.string().required(),
-    category: Joi.string().required()
-	});
-	return schema.validate(data, { abortEarly: false });
-  }
-  module.exports.Product = Product;
-  module.exports.validate = validateProduct;
+module.exports = Product;
